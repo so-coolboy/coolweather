43fc46da26a17e014cfac5f3e564fbd020ed9f11
so-coolboy <1364707232@qq.com>
2017-03-11 22:16:58 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/xck/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.xck.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by www on 2017/3/11.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
